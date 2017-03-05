ddd44069749b90ed3f8205fd1a17b9910d93b792
KabusIblis <kabusiblis@gmail.com>
2017-03-05 16:57:37 +0000

    gamemode files

---
+++ gamemode/redream/gamemode/shared.lua
@@ -0,0 +1,9 @@
+GM.Name = "Re-Dream"
+GM.Author = "Re-Dream Community"
+GM.Website = "-"
+GM.Version = "-1"
+
+DeriveGamemode("sandbox")
+DEFINE_BASECLASS("gamemode_sandbox")
+
+GM.Sandbox = BaseClass
