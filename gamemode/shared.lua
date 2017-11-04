def6d11c179fdb3e20b882e646cfbd0bc80f4d34
Tenrys <tenrys.iaido@live.fr>
2017-11-04 12:45:11 +0100

    add website

--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -1,9 +1,11 @@
+
 GM.Name = "Re-Dream"
 GM.Author = "Re-Dream Community"
-GM.Website = "-"
+GM.Website = "https://gmlounge.us/redream"
 GM.Version = "-1"
 
 DeriveGamemode("sandbox")
 DEFINE_BASECLASS("gamemode_sandbox")
 
 GM.Sandbox = BaseClass
+
